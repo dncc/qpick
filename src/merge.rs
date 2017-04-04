aea46ceff1bd663d6afa212ca495b7770ed8fa07
Dragan Cvetinovic <dcvetinovic@gmail.com>
2017-04-04 11:31:44 +0200

    Convert shard map keys to global keys and vice versa

--- src/merge.rs
+++ src/merge.rs
@@ -2,6 +2,36 @@ use std::fs::File;
 use std::io::BufWriter;
 use fst::{Error, raw, Streamer};
 
+static SEPARATOR: &'static str = "\u{0}";
+
+#[inline]
+fn npid2key(ngramv: & mut Vec<u8>, pid: usize) -> String {
+    let n = String::from_utf8_lossy(&ngramv).into_owned();
+    let k = format!("{}{}{}", n, SEPARATOR, pid);
+
+    if key2npid(&k) != (n.clone(), pid) {
+        panic!("Failed to turn {} and {} into key {}", n, pid, &k);
+    };
+
+    k
+}
+
+#[inline]
+fn key2npid(key: &str) -> (String, usize) {
+
+    let mut split = key.split(SEPARATOR);
+    let v = split.collect::<Vec<&str>>();
+
+    assert_eq!(v.len(), 2);
+
+    let ngram = v[0].to_string();
+    let pid = v[1].parse::<usize>().unwrap();
+
+    (ngram, pid)
+}
+
+
+
 fn merge(dir_path: String, nr_shards: usize) -> Result<(), Error>{
     let mut fsts = vec![];
     for i in 0..nr_shards {
@@ -14,7 +44,17 @@ fn merge(dir_path: String, nr_shards: usize) -> Result<(), Error>{
     let mut builder = try!(raw::Builder::new(wtr));
 
     while let Some((k, vs)) = union.next() {
-        try!(builder.insert(k, vs[0].value));
+        // v = [IndexValue{index:0, value: 1}, IndexValue{index:1, value: 0}, ... ]
+        let mut v = vs.to_vec();
+        // has to be sorted by index, it is sorted by value by default
+        v.sort_by(|a, b| a.index.partial_cmp(&b.index).unwrap());
+
+        let ref mut kv = k.to_vec();
+        for iv in v.iter() {
+            let k = npid2key(kv, iv.index);
+            builder.insert(k, iv.value).unwrap();
+        }
+
     }
     try!(builder.finish());
 
