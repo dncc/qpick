adf79860aa0aee831d361e4908da7968fb37c395
Dragan Cvetinovic <dcvetinovic@gmail.com>
2019-02-05 11:32:57 +0100

    Include ngrams with terms relevance above threshold

--- src/ngrams.rs
+++ src/ngrams.rs
@@ -7,13 +7,28 @@ use std::collections::HashSet;
 
 use shard::QueryType;
 
-macro_rules! bow_ngrams {
-    ($wv: ident, $ngrams: ident, $mult: ident) => {
-        if $wv.len() > 1 {
-            for i in 0..$wv.len() - 1 {
-                let (w1, mut w2) = (&$wv[i].0, &$wv[i + 1].0);
-                let mut tr = $mult * ($wv[i].1 + $wv[i + 1].1);
-                let mut v = String::with_capacity(w1.len() + w2.len() + 1);
+#[inline]
+fn bow_ngrams(words: Vec<(String, f32)>, ngrams: &mut HashMap<String, f32>, mult: f32) {
+    if words.len() > 1 {
+        for i in 0..words.len() - 1 {
+            let (w1, mut w2) = (&words[i].0, &words[i + 1].0);
+            let mut tr = mult * (words[i].1 + words[i + 1].1);
+            let mut v = String::with_capacity(w1.len() + w2.len() + 1);
+            if w1 < w2 {
+                v.push_str(w1);
+                v.push_str(" ");
+                v.push_str(w2);
+            } else {
+                v.push_str(w2);
+                v.push_str(" ");
+                v.push_str(w1);
+            }
+            ngrams.insert(v, tr);
+
+            if i < words.len() - 2 {
+                w2 = &words[i + 2].0;
+                tr = 0.97 * (words[i].1 + words[i + 2].1);
+                v = String::with_capacity(w1.len() + w2.len() + 1);
                 if w1 < w2 {
                     v.push_str(w1);
                     v.push_str(" ");
@@ -23,12 +38,22 @@ macro_rules! bow_ngrams {
                     v.push_str(" ");
                     v.push_str(w1);
                 }
-                $ngrams.insert(v, tr);
+                ngrams.insert(v, tr);
+            }
+        }
 
-                if i < $wv.len() - 2 {
-                    w2 = &$wv[i + 2].0;
-                    tr = 0.97 * ($wv[i].1 + $wv[i + 2].1);
-                    v = String::with_capacity(w1.len() + w2.len() + 1);
+        if words.len() > 3 {
+            let tr_threshold = 2.0 / words.len() as f32;
+            for i in 0..words.len() - 1 {
+                for j in i + 1..words.len() {
+                    let mut tr = words[i].1 + words[j].1;
+                    if tr < tr_threshold {
+                        continue;
+                    }
+
+                    let (w1, mut w2) = (&words[i].0, &words[j].0);
+                    let mut v = String::with_capacity(w1.len() + w2.len() + 1);
+
                     if w1 < w2 {
                         v.push_str(w1);
                         v.push_str(" ");
@@ -38,33 +63,38 @@ macro_rules! bow_ngrams {
                         v.push_str(" ");
                         v.push_str(w1);
                     }
-                    $ngrams.insert(v, tr);
-                }
-            }
 
-            // the first and the last term only for titles (bow, len=4)
-            if $wv.len() > 3 && $mult < 1.0 {
-                let (t1, t2) = (&$wv[0], &$wv[&$wv.len() - 1]);
-                let (w1, w2) = (&t1.0, &t2.0);
-                let tr = $mult * (t1.1 + t2.1);
-                let mut v = String::with_capacity(w1.len() + w2.len() + 1);
-                if w1 < w2 {
-                    v.push_str(w1);
-                    v.push_str(" ");
-                    v.push_str(w2);
-                } else {
-                    v.push_str(w2);
-                    v.push_str(" ");
-                    v.push_str(w1);
+                    if ngrams.contains_key(&v) {
+                        continue;
+                    }
+
+                    ngrams.insert(v, tr);
                 }
-                $ngrams.insert(v, tr);
             }
-        } else if $wv.len() == 1 {
-            let w = $wv[0].0.clone();
-            let tr = $mult * $wv[0].1;
-            $ngrams.insert(w, tr);
         }
-    };
+
+        // the first and the last term only for titles (bow, len=4)
+        if words.len() > 3 && mult < 1.0 {
+            let (t1, t2) = (&words[0], &words[&words.len() - 1]);
+            let (w1, w2) = (&t1.0, &t2.0);
+            let tr = mult * (t1.1 + t2.1);
+            let mut v = String::with_capacity(w1.len() + w2.len() + 1);
+            if w1 < w2 {
+                v.push_str(w1);
+                v.push_str(" ");
+                v.push_str(w2);
+            } else {
+                v.push_str(w2);
+                v.push_str(" ");
+                v.push_str(w1);
+            }
+            ngrams.insert(v, tr);
+        }
+    } else if words.len() == 1 {
+        let w = words[0].0.clone();
+        let tr = mult * words[0].1;
+        ngrams.insert(w, tr);
+    }
 }
 
 const RM_SYMBOLS: &str = "@#!";
@@ -87,6 +117,7 @@ fn fold_to_ngram(terms: Vec<String>, terms_relevance: &HashMap<String, f32>) -> 
     (s, r)
 }
 
+#[inline]
 fn get_terms_relevance(terms: &Vec<String>, tr_map: &fst::Map) -> HashMap<String, f32> {
     let mut missing: HashSet<String> = HashSet::new();
     let mut terms_rel: HashMap<String, f32> = HashMap::new();
@@ -126,6 +157,7 @@ fn get_terms_relevance(terms: &Vec<String>, tr_map: &fst::Map) -> HashMap<String
     terms_rel
 }
 
+#[inline]
 pub fn parse(
     query: &str,
     stopwords: &HashSet<String>,
@@ -190,8 +222,8 @@ pub fn parse(
     };
 
     // generate ngrams as bag of words combination of terms c a b d -> ac, bc, ab, ad, bd
-    bow_ngrams!(w_stop, ngrams, mult);
-    bow_ngrams!(wo_stop, ngrams, mult);
+    bow_ngrams(w_stop, &mut ngrams, mult);
+    bow_ngrams(wo_stop, &mut ngrams, mult);
 
     return ngrams;
 }
@@ -272,6 +304,54 @@ mod tests {
             ("account login".to_string(), 0.4),
             ("1 und 1 login".to_string(), 0.776),
             ("1 und 1 account".to_string(), 0.8),
+        ].iter()
+            .cloned()
+            .collect();
+
+        assert_eq!(
+            parse(query, &stopwords, &terms_relevance, QueryType::Q),
+            expected
+        );
+    }
+
+    #[test]
+    fn test_parse_long_query() {
+        let query = "what i learned from 200 job interviews at google";
+
+        let stopwords: HashSet<String> = ["what".to_string(), "from".to_string(), "at".to_string()]
+            .iter()
+            .cloned()
+            .collect();
+
+        let terms_relevance = Map::from_iter(vec![
+            ("200", 1),
+            ("google", 1),
+            ("i", 1),
+            ("interviews", 1),
+            ("job", 1),
+            ("learned", 1),
+        ]).unwrap();
+
+        let expected: HashMap<String, f32> = [
+            ("from 200 what i learned".to_string(), 0.5555556),
+            ("200 job".to_string(), 0.22222222),
+            ("from 200 job".to_string(), 0.33333334),
+            ("200 learned".to_string(), 0.22222222),
+            ("learned".to_string(), 0.11111111),
+            ("interviews".to_string(), 0.11111111),
+            ("job what i learned".to_string(), 0.43111113),
+            ("google job".to_string(), 0.21555556),
+            ("google".to_string(), 0.11111111),
+            ("at google interviews".to_string(), 0.33333334),
+            ("at google what i learned".to_string(), 0.5555556), // tr > 0.5
+            ("at google job".to_string(), 0.32333335),
+            ("200".to_string(), 0.11111111),
+            ("from 200 interviews".to_string(), 0.32333335),
+            ("google interviews".to_string(), 0.22222222),
+            ("job".to_string(), 0.11111111),
+            ("200 interviews".to_string(), 0.21555556),
+            ("interviews job".to_string(), 0.22222222),
+            ("job learned".to_string(), 0.21555556),
         ].iter()
             .cloned()
             .collect();
